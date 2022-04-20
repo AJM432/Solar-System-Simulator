3f7c93205943dec803305a3cf88f0f25191ca177
AJM432 <49791407+AJM432@users.noreply.github.com>
2022-04-20 19:53:49 -0400

    Update universe.py

--- universe.py
+++ universe.py
@@ -21,7 +21,7 @@ PALE_YELLOW = (240, 240, 0)
 # background_orbit_paths = np.array([[x, y] for x in range(0, WIDTH) for y in range(0, HEIGHT)])
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Collision Simulation")
+pygame.display.set_caption("Gravity Simulation")
 clock = pygame.time.Clock()
 
 background_orbit_paths_array = np.zeros((WIDTH, HEIGHT, 3), dtype=np.uint8)
@@ -35,49 +35,67 @@ class Vector:
     def magnitude(self):
         return (self.x**2 + self.y**2)**(1/2)
 
-    def add(vec_1, vec_2):
-        return Vector(vec_1.x + vec_2.x, vec_1.y + vec_2.y)
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
 
-    def subtract(vec_1, vec_2):
-        return Vector.add(vec_1, Vector.multiply_scaler(vec_2, -1))
+    def __sub__(self, other):
+        return self + -1*other
 
-    def multiply_scaler(vec, scalar):
-        return Vector(vec.x*scalar, vec.y*scalar)
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
 
-    def display_components(self):
-        return (self.x, self.y)
+    def __rmul__(self, scalar): # scalar must be on left side (scalar*Vector)
+        return Vector(self.x*scalar, self.y*scalar)
 
     def distance(vec_1, vec_2): # can be used for distance between points as well
-        return Vector.subtract(vec_2, vec_1).magnitude()
+        return (vec_2 - vec_1).magnitude()
 
     def angle_to_vertical(self):
         return math.atan(self.y/self.x)
         # return math.atan2(self.y, self.x)
 
+a= Vector(1, 2)
+print(5*a)
 class CelestialBody:
     def __init__(self, name, mass, x, y, vx, vy, color, radius, is_influenced=True):
         self.mass = mass
         self.name = name
-        self.x = x
-        self.y = y
         self.vx = vx
         self.vy = vy
+        self.x = x
+        self.y = y
         self.color = color
         self.radius = radius
         self.is_influenced = is_influenced
 
     def get_gravitational_velocity_vec(self, object_b): # add to initial velocity vector
-        force_magnitude = object_b.mass/Vector.distance(self, object_b)**2*(1/FPS) # vf_a=M_b/r^2
-        force_direction = Vector.subtract(object_b, self)
-        # angle = Vector.subtract(self, object_b).angle_to_vertical()
+
+        # # Integral method
+        # #__________________
+        # current_distance = Vector.distance(self, object_b)
+        # self.x -= self.vx
+        # self.y -= self.vy
+        # last_distance = Vector.distance(self, object_b)
+        # self.x += self.vx
+        # self.y += self.vy
+        # # force_magnitude = (-object_b.mass/(2*current_distance) - (-object_b.mass/(2*last_distance)))*(1/FPS)
+        # #__________________
+
+        force_magnitude = object_b.mass/Vector.distance(self.get_vector(), object_b.get_vector())**2*(1/FPS) # vf_a=M_b/r^2
+
+
+
+        force_direction = object_b.get_vector() - self.get_vector()
         
-        # return Vector(force_direction.x/force_direction.magnitude()*force_magnitude, force_direction.y/force_direction.magnitude()*force_magnitude)
-        return Vector.multiply_scaler(force_direction, (1/force_direction.magnitude())*force_magnitude)
+        return ((1/force_direction.magnitude())*force_magnitude)*force_direction
 
     def update_orbit_velocity(self, object_b):
         gravity_vec = self.get_gravitational_velocity_vec(object_b)
         self.vx += gravity_vec.x
         self.vy += gravity_vec.y
+
+    def get_vector(self):
+        return Vector(self.x, self.y)
 
     def out_of_bounds(self):
         if self.x <= 0 or self.x + self.radius*2 >= WIDTH:
@@ -125,16 +143,48 @@ class CelestialBody:
 #     'neptune' : CelestialBody(name='neptune', mass=1500, x=WIDTH//2-600, y=HEIGHT//2, vx=0, vy=-1, color=BLUE, radius=12),
 #     }
 
+# solar_system = {
+#     'sun' : CelestialBody(name='sun', mass=500000, x=WIDTH//2, y=HEIGHT//2, vx=0, vy=0, color=YELLOW, radius=30, is_influenced=True),
+#     'sun2' : CelestialBody(name='sun2', mass=50000, x=WIDTH//2-201, y=HEIGHT//2, vx=0, vy=5, color=YELLOW, radius=30),
+#     'mercury' : CelestialBody(name='mercury', mass=10, x=WIDTH//2-50, y=HEIGHT//2, vx=0, vy=-12, color=WHITE, radius=3),
+#     'venus' : CelestialBody(name='venus', mass=10, x=WIDTH//2-100, y=HEIGHT//2-3, vx=0, vy= 8, color=ORANGE, radius=7),
+#     'earth' : CelestialBody(name='earth', mass=10, x=WIDTH//2-150, y=HEIGHT//2, vx=0, vy=-6, color=BLUE, radius=7),
+#     'mars' : CelestialBody(name='mars', mass=10, x=WIDTH//2-200, y=HEIGHT//2, vx=0, vy=-5, color=RED, radius=5),
+#     'jupiter' : CelestialBody(name='jupiter', mass=10, x=WIDTH//2-300, y=HEIGHT//2, vx=0, vy=-4, color=RED, radius=15),
+#     'saturn' : CelestialBody(name='saturn', mass=10, x=WIDTH//2-400, y=HEIGHT//2, vx=0, vy=-3.5, color=PALE_YELLOW, radius=13),
+#     'uranus' : CelestialBody(name='uranus', mass=10, x=WIDTH//2-500, y=HEIGHT//2, vx=0, vy=-3, color=BLUE, radius=11),
+#     'neptune' : CelestialBody(name='neptune', mass=10, x=WIDTH//2-600, y=HEIGHT//2, vx=0, vy=-2.5, color=BLUE, radius=11),
+#     }
+
+
+
+# x1 = WIDTH//2
+# y1 = HEIGHT//2
+# x2 = WIDTH//2-100
+# y2 = y=HEIGHT//2-3
+# mb = 2000000
+# b=0
+# t=1
+# r = Vector.distance(Vector(x1, y1), Vector(x2, y2))
+
+# c= ((x1-x2)*((mb*t)/(r**3)+1))
+# d= ((y1-y2)*((mb*t)/(r**3)+1))
+# v_1 = (-2*c + math.sqrt((2*c)**2 -4*(-r**2 + c**2 + d**2 + 2*b*d+b**2)))/2
+# v_2 = (-2*c + math.sqrt((2*c)**2 -4*(-r**2 + c**2 + d**2 + 2*b*d+b**2)))/2
+
+
+
+
 solar_system = {
-    'sun' : CelestialBody(name='sun', mass=300000, x=WIDTH//2, y=HEIGHT//2, vx=0, vy=0, color=YELLOW, radius=40, is_influenced=False),
-    'mercury' : CelestialBody(name='mercury', mass=0, x=WIDTH//2-50, y=HEIGHT//2, vx=0, vy=-11, color=WHITE, radius=3),
-    'venus' : CelestialBody(name='venus', mass=0, x=WIDTH//2-100, y=HEIGHT//2-3, vx=0, vy=-8, color=ORANGE, radius=7),
-    'earth' : CelestialBody(name='earth', mass=0, x=WIDTH//2-150, y=HEIGHT//2, vx=0, vy=-6, color=BLUE, radius=7),
-    'mars' : CelestialBody(name='mars', mass=0, x=WIDTH//2-200, y=HEIGHT//2, vx=0, vy=-5, color=RED, radius=5),
-    'jupiter' : CelestialBody(name='jupiter', mass=0, x=WIDTH//2-300, y=HEIGHT//2, vx=0, vy=-4, color=RED, radius=20),
-    'saturn' : CelestialBody(name='saturn', mass=0, x=WIDTH//2-400, y=HEIGHT//2, vx=0, vy=-3.5, color=PALE_YELLOW, radius=16),
-    'uranus' : CelestialBody(name='uranus', mass=0, x=WIDTH//2-500, y=HEIGHT//2, vx=0, vy=-3, color=BLUE, radius=12),
-    'neptune' : CelestialBody(name='neptune', mass=0, x=WIDTH//2-600, y=HEIGHT//2, vx=0, vy=-2.5, color=BLUE, radius=12),
+    'sun' : CelestialBody(name='sun', mass=200000, x=WIDTH//2, y=HEIGHT//2, vx=0, vy=0, color=YELLOW, radius=10, is_influenced=False),
+    'mercury' : CelestialBody(name='mercury', mass=200, x=WIDTH//2-50, y=HEIGHT//2, vx=0, vy=6, color=WHITE, radius=3),
+    'venus' : CelestialBody(name='venus', mass=200, x=WIDTH//2-100, y=HEIGHT//2-3, vx=0, vy= 4, color=ORANGE, radius=3),
+    'earth' : CelestialBody(name='earth', mass=200, x=WIDTH//2-150, y=HEIGHT//2, vx=0, vy=-4, color=BLUE, radius=7),
+    'mars' : CelestialBody(name='mars', mass=200, x=WIDTH//2-200, y=HEIGHT//2, vx=0, vy=-3, color=RED, radius=5),
+    'jupiter' : CelestialBody(name='jupiter', mass=200, x=WIDTH//2-300, y=HEIGHT//2, vx=0, vy=-3, color=RED, radius=15),
+    'saturn' : CelestialBody(name='saturn', mass=200, x=WIDTH//2-400, y=HEIGHT//2, vx=0, vy=-2, color=PALE_YELLOW, radius=13),
+    'uranus' : CelestialBody(name='uranus', mass=200, x=WIDTH//2-500, y=HEIGHT//2, vx=0, vy=-2, color=BLUE, radius=11),
+    'neptune' : CelestialBody(name='neptune', mass=200, x=WIDTH//2-600, y=HEIGHT//2, vx=0, vy=-1, color=BLUE, radius=11)
     }
 
 running = True
