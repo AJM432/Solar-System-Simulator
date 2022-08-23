56084ac182604c201c4cf89c61824c1a4b2e45a2
AJM432 <49791407+AJM432@users.noreply.github.com>
2022-08-22 22:02:13 -0400

    added constants file

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 import pygame
 import numpy as np
+from constants import *
 
 pygame.init()
-# WIDTH = HEIGHT = 800
-WIDTH, HEIGHT = 1400, 800
-FPS = 60
-ZOOM_FACTOR = 1
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 190, 0)
-ORANGE = (255, 215, 0)
-GREY = (125, 125, 125)
-PALE_YELLOW = (240, 240, 0)
-PINK = (255,192,203)
-BACKGROUND_COLOR = BLACK
-
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gravity Simulation")
